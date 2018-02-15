2edc038a286cf632dea1f6b5d57a24f3c38f17ab
BoykoNeov <boikoneov@gmail.com>
2018-02-15 17:58:45 +0200

    :muscle: strted work on "Cat Lady"

---
+++ DefiningClasses/CatLady/Launcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Launcher
+{
+    public static void Main()
+    {
+        Siamese a = new Siamese("test", 4);
+        a.ToString();
+        Console.WriteLine(a);
+    }
+}
---
+++ DefiningClasses/CatLady/Siamese.cs
@@ -0,0 +1,23 @@
+﻿public class Siamese : Cat
+{
+    public override string Name { get; set; }
+    public int EarSize { get; set; }
+
+    public Siamese() : base()
+    {
+    }
+
+    private Siamese(string name) : base(name)
+    {
+    }
+
+    public Siamese(string name, int earSize) : this(name)
+    {
+        this.EarSize = earSize;
+    }
+
+    public override string ToString()
+    {
+        return(this.GetType().Name + " " + this.Name + " " + this.EarSize);
+    }
+}
