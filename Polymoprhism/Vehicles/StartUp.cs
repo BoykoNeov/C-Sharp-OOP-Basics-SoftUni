f1a6f1404cbc81e6884985d546da34a4b93f6533
BoykoNeov <boikoneov@gmail.com>
2018-03-02 14:54:59 +0200

    :chestnut: started work on "Polymoprhism", added relevant files

---
+++ Polymoprhism/Vehicles/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        string[] carParams = Console.ReadLine().Split();
+        Car car = new Car(double.Parse(carParams[1]), double.Parse(carParams[2]));
+
+        string[] truckParams = Console.ReadLine().Split();
+        Truck truck = new Truck(double.Parse(truckParams[1]), double.Parse(truckParams[2]));
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] inputCmds = Console.ReadLine().Split();
+
+            Vehicle vehicle = null;
+
+            if (inputCmds[1] == "Car")
+            {
+                vehicle = car;
+            }
+            else if (inputCmds[1] == "Truck")
+            {
+                vehicle = truck;
+            }
+
+            Type thisType = vehicle.GetType();
+            MethodInfo theMethod = thisType.GetMethod(inputCmds[0]);
+            object[] methodParams = new object[] { double.Parse(inputCmds[2]) };
+            object test = theMethod.Invoke(vehicle, methodParams);
+        }
+    }
+}
