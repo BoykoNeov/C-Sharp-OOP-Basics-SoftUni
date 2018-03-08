2b95cd6594f62ff81434e15d686e372ad6756e7b
BoykoNeov <boikoneov@gmail.com>
2018-03-08 22:53:48 +0200

    :green_apple: started work on "workshop"

---
+++ Forum.Models/Category.cs
@@ -0,0 +1,31 @@
+﻿namespace Forum.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Category
+    {
+        private int id;
+        private string name;
+        private ICollection<int> postIds;
+
+        public ICollection<int> PostIds
+        {
+            get { return postIds; }
+            set { postIds = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
---
+++ Forum.Models/User.cs
@@ -0,0 +1,38 @@
+﻿namespace Forum.Models
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        private int id;
+        private string password;
+        private string name;
+        private ICollection<int> postIds;
+
+        public ICollection<int> PostIds
+        {
+            get { return postIds; }
+            set { postIds = value; }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
